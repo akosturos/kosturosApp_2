35b0762b03907cc0129226658ed596402030a3fd
Tony Kosturos <kosturosanthony@gmail.com>
2016-02-28 13:45:09 -0800

    Finish static pages

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,14 +1,28 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Kosturos Foundation"
+  end
+
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help – #{@base_title}"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About – #{@base_title}"
   end
 
 end
